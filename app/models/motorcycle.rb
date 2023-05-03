108586a20f4e4df4a27af10152607794a2942e85
belaymit <2belamit@gmail.com>
2023-05-03 10:18:54 +0300

    Added validations

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,2 +1,9 @@
 class Motorcycle < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_many :users, through: :reservations, dependent: :destroy
+
+  validates :name, :price, :description, :image, :category, presence: true
+  validates :name, :category, length: { maximum: 100 }
+  validates :image, :description, length: { maximum: 400 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
